db8403d276998609b6ae1cf4be22b5077a37f6b9
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-04-06 11:45:58 +0200

    Change marker color

--- app/src/main/java/com/shafigh/queuesystem/MapsActivity.kt
+++ app/src/main/java/com/shafigh/queuesystem/MapsActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
@@ -103,6 +104,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     .position(latLng)
                     .title(getString(R.string.dropped_pin))
                     .snippet(snippet)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
             )
         }
     }
