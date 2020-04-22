1df6dd904ed65e163ce45cbfae9a1a9d6c520070
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-04-22 02:34:02 +0200

    Implement search field

--- app/src/main/java/com/shafigh/queuesystem/MapsActivity.kt
+++ app/src/main/java/com/shafigh/queuesystem/MapsActivity.kt
@@ -1,24 +1,40 @@
 package com.shafigh.queuesystem
 
+import android.content.Context
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.view.Menu
 import android.view.MenuItem
+import android.view.WindowManager
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.places.ui.PlacePicker
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import java.io.IOException
 import java.util.*
 
@@ -27,19 +43,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var map: GoogleMap
 
     ///Users/shafighkhalili/Library/Android/sdk/sources/android-29/java/security/KeyStore.java
-    private val TAG = MapsActivity::class.java.simpleName
+    //private val TAG = MapsActivity::class.java.simpleName
 
     private val REQUEST_LOCATION_PERMISSION = 1
     private val PLACE_PICKER_REQUEST = 3
-
+    private val TAG = "MapActivity"
+    private val DEFAULT_ZOOM = 15f
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null
+    private val mLocationPermissionsGranted = false
+    //Widgets
+    private lateinit var mSearchText: AutoCompleteTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        mSearchText = findViewById(R.id.input_search)
+
     }
 
     /**
@@ -51,14 +73,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    /* override fun onMapReady(googleMap: GoogleMap) {
-         map = googleMap
-
-         // Add a marker in Sydney and move the camera
-         val sydney = LatLng(-34.0, 151.0)
-         map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-         map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-     }*/
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
@@ -72,16 +86,64 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val overlaySize = 100F
 
-        val androidOverlay = GroundOverlayOptions()
+        /*val androidOverlay = GroundOverlayOptions()
             .image(BitmapDescriptorFactory.fromResource((R.drawable.android)))
-            .position(homeLatLng, overlaySize)
-        map.addGroundOverlay(androidOverlay)
+            .position(homeLatLng, overlaySize)*/
+//        map.addGroundOverlay(androidOverlay)
         map.addMarker(MarkerOptions().position(homeLatLng))
 
         setMapLongClick(map)
         setPoiClick(map)
         setMapStyle(map)
         enableMyLocation()
+        init()
+        setPadding()
+        initAutocomplete()
+    }
+    //Autocomplete test
+    private fun initAutocomplete(){
+        // Initialize the AutocompleteSupportFragment.
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment: AutocompleteSupportFragment? =
+            supportFragmentManager.findFragmentById(R.id.input_search) as AutocompleteSupportFragment?
+
+        // Specify the types of place data to return.
+
+        // Specify the types of place data to return.
+        autocompleteFragment?.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment?.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i(
+                    TAG,
+                    "Place: " + place.name.toString() + ", " + place.id
+                )
+            }
+
+            override fun onError(p0: Status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: $p0")
+            }
+        })
+    }
+    private fun init() {
+        //Enable Enter keyboard key
+        hideKeyboard()
+        mSearchText.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                actionId == EditorInfo.IME_ACTION_DONE ||
+                event.action == KeyEvent.ACTION_DOWN ||
+                event.action == KeyEvent.KEYCODE_ENTER
+            ) {
+                //Execute out method for searching
+                geoLocate()
+            }
+            return@setOnEditorActionListener false
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -156,16 +218,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun isPermissionGranted() : Boolean {
+    private fun isPermissionGranted(): Boolean {
         return ContextCompat.checkSelfPermission(
             this,
-            android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun enableMyLocation(){
-        if(isPermissionGranted()){
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
             map.isMyLocationEnabled = true
-        }else{
+        } else {
             ActivityCompat.requestPermissions(
                 this, arrayOf<String>(android.Manifest.permission.ACCESS_FINE_LOCATION),
                 REQUEST_LOCATION_PERMISSION
@@ -176,7 +239,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
-        grantResults: IntArray) {
+        grantResults: IntArray
+    ) {
         if (requestCode == REQUEST_LOCATION_PERMISSION) {
             if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
                 enableMyLocation()
@@ -211,7 +275,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             if (null != addresses && !addresses.isEmpty()) {
                 address = addresses[0]
                 for (i in 0 until address.maxAddressLineIndex) {
-                    addressText += if (i == 0) address.getAddressLine(i) else "\n" + address.getAddressLine(i)
+                    addressText += if (i == 0) address.getAddressLine(i) else "\n" + address.getAddressLine(
+                        i
+                    )
                 }
             }
         } catch (e: IOException) {
@@ -220,5 +286,75 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         return addressText
     }
+
+    private fun geoLocate() {
+        Log.d(TAG, "geoLocate: geolocating")
+        val searchString = mSearchText.text.toString()
+        val geocoder = Geocoder(this@MapsActivity)
+        var list: List<Address> =
+            ArrayList()
+        try {
+            list = geocoder.getFromLocationName(searchString, 1)
+        } catch (e: IOException) {
+            Log.e(
+                TAG,
+                "geoLocate: IOException: " + e.message
+            )
+        }
+        if (list.isNotEmpty()) {
+            val address = list[0]
+            Log.d(TAG, "geoLocate: found a location: $address")
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+            moveCamera(
+                LatLng(address.latitude, address.longitude), DEFAULT_ZOOM, address.getAddressLine(0)
+            )
+        }
+    }
+
+    private fun moveCamera(latLng: LatLng, zoom: Float, title: String) {
+        Log.d(
+            TAG,
+            "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude
+        )
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom))
+        if (title != "My Location") {
+            val options = MarkerOptions()
+                .position(latLng)
+                .title(title)
+            map.addMarker(options)
+        }
+        hideKeyboard()
+    }
+
+    //Hide keyboard
+    private fun AppCompatActivity.hideKeyboard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+        // else {
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+        // }
+    }
+
+    private fun getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting the devices current location")
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        try {
+            if (map.isMyLocationEnabled) {
+                mFusedLocationProviderClient?.lastLocation?.addOnCompleteListener()
+            }
+        } catch (e: SecurityException) {
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.message)
+        }
+    }
+
+    private fun setPadding(){
+        map.setPadding(0,200,0,0)
+    }
+}
+
+private fun <TResult> Task<TResult>?.addOnCompleteListener() {
 
 }
